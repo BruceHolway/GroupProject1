5fef2157b26fdd32cf9e903b987ce935e045e592
Jayden Connerly <18.jayden.connerly@ksd.org>
2019-10-31 09:08:57 -0700

    script schtuff

---
+++ 2DGroupProjectGame/Assets/Scripts/CoinCollectAndAddHP.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CoinCollectAndAddHP : MonoBehaviour
+{
+    public int Coin = 0;
+    public int goal;
+    public Text cointext;
+    public string player;
+
+    void Start()
+    {
+        cointext.text = "COINS " + Coin + "/" + goal;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "coin")
+        {
+            Coin++;
+            GameObject.Find(player).GetComponent<PlayerHP>().health++;
+            GameObject.Find(player).GetComponent<PlayerHP>().Healthupdate();
+            
+            cointext.text = "coins " + Coin + "/" + goal;
+            Destroy(collision.gameObject);
+            if (Coin > goal - 1)
+            {
+                SceneManager.LoadScene("win");
+            }
+        }
+        
+    }
+}
+
---
+++ 2DGroupProjectGame/Assets/Scripts/PlayerHPReferencable.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerHPReferencable: MonoBehaviour
+{
+    public int health;
+    public int maxhp;
+    public Text hptext;
+    public Slider hpslider;
+    public int lives;
+
+    void Start()
+    {
+        lives = PlayerPrefs.GetInt("lives");
+        hpslider.maxValue = maxhp;
+        hpslider.value = health;
+        hptext.text = "HP " + health + "/" + maxhp;
+        GetComponent<Animator>().SetFloat("hp", health);
+    }
+
+    void Update()
+    {
+       if (health > maxhp)
+        {
+            health = maxhp;
+            Healthupdate();
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "enemy")
+        {
+            hurt();
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "enemy bullet")
+        {
+            hurt();
+        }
+    }
+    public void hurt()
+    {
+        health--;
+        GetComponent<Animator>().SetFloat("hp", health);
+        Healthupdate();
+        if (health < 1)
+        {
+            if (lives > 0)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                PlayerPrefs.SetInt("lives", lives - 1);
+            }
+            else
+            {
+                SceneManager.LoadScene("gameover");
+            }
+        }
+    }
+    public void Healthupdate()
+    {
+        hpslider.value = health;
+        hptext.text = "HP" + health + "/" + maxhp;
+    }
+}
