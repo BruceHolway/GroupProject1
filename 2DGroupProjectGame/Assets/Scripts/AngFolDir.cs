4a53f1fc7502ce63b2a9fe8374ead7c31f046bb5
Jayden Connerly <18.jayden.connerly@ksd.org>
2019-11-06 10:00:18 -0800

    angfoldir

---
+++ 2DGroupProjectGame/Assets/Scripts/AngFolDir.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AngFolDir : MonoBehaviour
+{
+    Vector3 lastpos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastpos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveDirection = gameObject.transform.position - lastpos;
+        if (moveDirection != Vector3.zero)
+        {
+            float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+            lastpos = transform.position;
+        }
+    }
+}
