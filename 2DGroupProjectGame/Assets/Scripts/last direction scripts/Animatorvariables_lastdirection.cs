41c1811d0c17fed48e4d0e89bcadd535f373fe75
Jayden Connerly <18.jayden.connerly@ksd.org>
2019-11-06 09:47:11 -0800

    lastdirection scripts

---
+++ 2DGroupProjectGame/Assets/Scripts/last direction scripts/Animatorvariables_lastdirection.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animatorvariables_lastdirection : MonoBehaviour
+{
+    public float lastdirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastdirection = 0;
+        GetComponent<Animator>().SetFloat("lastdirection", lastdirection);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        GetComponent<Animator>().SetFloat("x", x);
+        GetComponent<Animator>().SetFloat("y", y);
+        if (x == -1)
+        {
+            if (y == -1)
+            {
+                lastdirection = 2.5f;
+            }
+            else if (y == 1)
+            {
+                lastdirection = 3.5f;
+            }
+            else if (y == 0)
+            {
+                lastdirection = 3;
+            }
+        }
+        else if (x == 1)
+        {
+            if (y == -1)
+            {
+                lastdirection = 0.5f;
+            }
+            else if (y == 1)
+            {
+                lastdirection = 1.5f;
+            }
+            else if (y == 0)
+            {
+                lastdirection = 1;
+            }
+        }
+        else if (x == 0)
+        {
+            if (y == -1)
+            {
+                lastdirection = 0;
+            }
+            else if (y == 1)
+            {
+                lastdirection = 2;
+            }
+            
+        }
+        GetComponent<Animator>().SetFloat("lastdirection", lastdirection);
+    }
+}
---
+++ 2DGroupProjectGame/Assets/Scripts/last direction scripts/shoot_facing.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot_facing : MonoBehaviour
+{
+    public GameObject prefab;
+    public float shootspeed = 10;
+    public float bulletlife = 1;
+    public float delay = 1.0f;
+    float timer = 0;
+    Vector2 shootdirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float direction = GetComponent<Animatorvariables>().lastdirection;
+
+        timer += Time.deltaTime;
+        
+        if (direction >= 0 && direction < 0.5)
+        {
+            shootdirection = new Vector2(0, -1);
+            Fire();
+
+        }
+        else if (direction >= 0.5 && direction < 1)
+        {
+            shootdirection = new Vector2(1, 0);
+            Fire();
+
+        }
+        else if (direction >= 1 && direction < 1.5)
+        {
+            shootdirection = new Vector2(1, 0);
+            Fire();
+
+        }
+        else if (direction >= 1.5 && direction < 2)
+        {
+            shootdirection = new Vector2(0, 1);
+            Fire();
+
+        }
+        else if (direction >= 2 && direction < 2.5)
+        {
+            shootdirection = new Vector2(0, 1);
+            Fire();
+
+        }
+        else if (direction >= 2.5 && direction < 3)
+        {
+            shootdirection = new Vector2(-1, 0);
+            Fire();
+
+        }
+        else if (direction >= 3 && direction < 3.5)
+        {
+            shootdirection = new Vector2(-1, 0);
+            Fire();
+
+        }
+        else if (direction >= 3.5 && direction < 3.5)
+        {
+            shootdirection = new Vector2(0, -1);
+            Fire();
+
+        }
+    }
+
+    public void Fire()
+    {
+        if (Input.GetKey(KeyCode.Z) && timer > delay)
+        {
+            timer = 0;
+            GameObject bullet = Instantiate(prefab, transform.position, Quaternion.identity);
+            //Vector3 mouseposition = Input.mousePosition;
+            //mouseposition = Camera.main.ScreenToWorldPoint(mouseposition);
+            //Vector2 shootdirection = new Vector2(mouseposition.x - transform.position.x, mouseposition.y - transform.position.y);
+            shootdirection.Normalize();
+            bullet.GetComponent<Rigidbody2D>().velocity = shootdirection * shootspeed;
+            Destroy(bullet, bulletlife);
+        }
+    }
+}
