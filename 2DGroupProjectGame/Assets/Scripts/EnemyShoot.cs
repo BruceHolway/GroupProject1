d895419b9605998400c3f150c607d23a0e04faf1
Bruce Holway <20.bruce.holway@ksd.org>
2019-11-15 09:37:46 -0800

    enemies in levels

--- 2DGroupProjectGame/Assets/Scripts/EnemyShoot.cs
+++ 2DGroupProjectGame/Assets/Scripts/EnemyShoot.cs
@@ -14,7 +14,7 @@ public class EnemyShoot : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
