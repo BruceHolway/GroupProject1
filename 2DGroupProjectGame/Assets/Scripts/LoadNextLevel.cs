59e63ae01e23b667b7ae2a25b762e3d6d52c1702
Jayden Connerly <18.jayden.connerly@ksd.org>
2019-11-18 08:27:35 -0800

    level loader

---
+++ 2DGroupProjectGame/Assets/Scripts/LoadNextLevel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextLevel : MonoBehaviour
+{
+    public string LevelName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            LoadLevel();
+        }
+    }
+
+    void LoadLevel()
+    {
+        SceneManager.LoadScene(LevelName);
+    }
+}
